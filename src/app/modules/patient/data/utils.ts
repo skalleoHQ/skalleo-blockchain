c7c36dfc84d18e2cfee5443894d697800b2f7b2e
moussa <ehpmtra@gmail.com>
2022-06-07 15:59:42 +0200

    utils.ts implemented

--- src/app/modules/patient/data/utils.ts
+++ src/app/modules/patient/data/utils.ts
@@ -3,7 +3,7 @@ import { codec, cryptography } from 'lisk-sdk';
 const VALID_PATIENT_DOMAIN = ['adh'];
 
 const registeredPatientAccountsSchema = {
-    $id: 'patient/registeredPatients',
+    $id: 'patient/registeredAccounts',
     type: 'object',
     required: ['registeredPatientAccounts'],
     properties: {
@@ -14,23 +14,102 @@ const registeredPatientAccountsSchema = {
                 type: 'object',
                 required: ['id', 'ownerAddress', 'patientIdentificationNumber', 'areaCode', 'username'],
                 properties: {
-                    
-                } 
-            }
-        }
-    }
-}
+                    id: {
+                        dataType: 'bytes',
+                        fieldNumber: 1,
+                    },
+                    ownerAddress: {
+                        dataType: 'bytes',
+                        fieldNumber: 2,
+                    },
+                    patientIdentificationNumber: {
+                        dataType: 'string',
+                        fieldNumber: 3,
+                    },
+                    areaCode: {
+                        dataType: 'string',
+                        fieldNumber: 4,
+                    },
+                    username: {
+                        dataType: 'string',
+                        fieldNumber: 5,
+                    },
+                }, 
+            },
+        },
+    },
+};
 
 
+const CHAIN_STATE_PATIENT_ACCOUNTS = 'patient: registeredPatientAccounts';
 
 
+const createPatientAccount = ({ patientIdentificationNumber, areaCode, username, ownerAddress, nonce }) => {
+    const nonceBuffer = Buffer.alloc(8);
+    nonceBuffer.writeBigInt64LE(nonce);
+
+    const seed = Buffer.concat([patientIdentificationNumber, areaCode, username, ownerAddress, nonceBuffer]);
+    const id = cryptography.hash(seed);
+
+    return {
+        id,
+        patientIdentificationNumber,
+        areaCode,
+        username,
+        ownerAddress,
+    };
+
+};
 
 
+const getAllPatientAccounts = async (stateStore) => {
+    const registeredAccountsBuffer = await stateStore.chain.get(
+        CHAIN_STATE_PATIENT_ACCOUNTS
+    );
+
+    if (!registeredAccountsBuffer) {
+        return [];
+    };
+
+    const registeredAccounts = codec.decode(
+        registeredPatientAccountsSchema,
+        registeredAccountsBuffer
+    );
 
 
+    return registeredAccounts.registeredPatientAccounts;
+};
 
 
+const getAllPatientAccountsAsJSON = async (dataAccess) => {
+    const registeredAccountsBuffer = await dataAccess.getChainState(
+        CHAIN_STATE_PATIENT_ACCOUNTS
+    );
 
+    if (!registeredAccountsBuffer) {
+        return [];
+    };
+
+    const registeredAccounts = codec.decode(
+        registeredPatientAccountsSchema,
+        registeredAccountsBuffer
+    );
+
+
+    return codec.toJSON(registeredPatientAccountsSchema, registeredAccounts).registeredPatientAccounts;
+};
+
+
+const setAllPatientAccounts = async (stateStore, patientAccounts) => {
+    const registeredAccounts = {
+        registeredPatientAccounts: patientAccounts.sort((a, b) => a.id.compare(b.id)),
+    };
+
+    await stateStore.chain.set(
+        CHAIN_STATE_PATIENT_ACCOUNTS,
+        codec.encode(registeredPatientAccountsSchema, registeredAccounts)
+    );
+};
 
 
 
@@ -40,5 +119,11 @@ const registeredPatientAccountsSchema = {
 
 module.exports = {
     VALID_PATIENT_DOMAIN,
+    registeredPatientAccountsSchema,
+    CHAIN_STATE_PATIENT_ACCOUNTS,
+    createPatientAccount,
+    getAllPatientAccounts,
+    setAllPatientAccounts,
+    getAllPatientAccountsAsJSON,
 }
 
