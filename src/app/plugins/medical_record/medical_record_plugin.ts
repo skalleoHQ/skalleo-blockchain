04a0d39b4546287d11afbf20597c6b28a5095842
moussa <ehpmtra@gmail.com>
2022-06-24 23:13:56 +0200

    medicalRecord channel in progress

--- src/app/plugins/medical_record/medical_record_plugin.ts
+++ src/app/plugins/medical_record/medical_record_plugin.ts
@@ -1,14 +1,34 @@
 import { BasePlugin, PluginInfo } from 'lisk-sdk';
 import type { BaseChannel, EventsDefinition, ActionsDefinition, SchemaWithDefault } from 'lisk-sdk';
 
+const express = require('express');
+const cors = require('cors');
+
+
+const { getDBInstance,
+		getPatientHistory,
+		getAllTransactions,
+		savePatientHistory,
+		savedTransactions,
+
+} = require('../medical_record/data/utils');
+
  /* eslint-disable class-methods-use-this */
  /* eslint-disable  @typescript-eslint/no-empty-function */
  export class MedicalRecordPlugin extends BasePlugin {
 	// private _channel!: BaseChannel;
+	_server = undefined;
+	_app = undefined;
+	_channel!: BaseChannel;
+	_db = undefined;
+	_nodeInfo = undefined;
+
+
 
 	public static get alias(): string {
 		return 'medicalRecord';
-	}
+	};
+
 
 	// eslint-disable-next-line @typescript-eslint/class-literal-property-style
 	public static get info(): PluginInfo {
@@ -17,7 +37,8 @@ import type { BaseChannel, EventsDefinition, ActionsDefinition, SchemaWithDefaul
 			version: '0.1.0',
 			name: 'medicalRecord',
 		};
-	}
+	};
+
 
 	// eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
 	public get defaults(): SchemaWithDefault {
@@ -28,25 +49,44 @@ import type { BaseChannel, EventsDefinition, ActionsDefinition, SchemaWithDefaul
 			required: [],
 			default: {},
 		}
-	}
+	};
+
 
 	public get events(): EventsDefinition {
 		return [
 			// 'block:created',
 			// 'block:missed'
 		];
-	}
+	};
+
 
 	public get actions(): ActionsDefinition {
 		return {
 		// 	hello: async () => { hello: 'world' },
 		};
-	}
+	};
+	
 
-		public async load(_: BaseChannel): Promise<void> {
+	public async load(channel: BaseChannel): Promise<void> {
+		this._app = express();
+		this._channel = channel;
+		this._db = await getDBInstance();
+		this._nodeInfo = await this._channel.invoke('app:getNodeInfo');
+		
+
+		this._app.use(cors({origin: '*', methods: ['GET', 'POST', 'PUT'] }));
+		this._app.use(express.join());
+
+		this._app.get('/api/patient_cares', async (req, res) => {
+			const patientAccounts = await this._channel.invoke('patient:getAllPatientAccounts');
+			
+		})
 		// this._channel = channel;
 		// this._channel.once('app:ready', () => {});
-	}
+	};
 
-	public async unload(): Promise<void> {}
+
+	public async unload(): Promise<void> {
+
+	}
 }
