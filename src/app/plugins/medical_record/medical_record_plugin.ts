57686707f0ff4f61f90c7c3aacd0d6fae98d0507
moussa <ehpmtra@gmail.com>
2022-06-25 02:38:01 +0200

    medicalRecord channel updated

--- src/app/plugins/medical_record/medical_record_plugin.ts
+++ src/app/plugins/medical_record/medical_record_plugin.ts
@@ -1,5 +1,6 @@
-import { BasePlugin, PluginInfo } from 'lisk-sdk';
+import { BasePlugin, codec, PluginInfo } from 'lisk-sdk';
 import type { BaseChannel, EventsDefinition, ActionsDefinition, SchemaWithDefault } from 'lisk-sdk';
+import { ARecordedCare } from '../../modules/professional/assets/register';
 
 const express = require('express');
 const cors = require('cors');
@@ -9,19 +10,19 @@ const { getDBInstance,
 		getPatientHistory,
 		getAllTransactions,
 		savePatientHistory,
-		savedTransactions,
+		saveTransactions,
 
 } = require('../medical_record/data/utils');
 
  /* eslint-disable class-methods-use-this */
  /* eslint-disable  @typescript-eslint/no-empty-function */
- export class MedicalRecordPlugin extends BasePlugin {
+export class MedicalRecordPlugin extends BasePlugin {
 	// private _channel!: BaseChannel;
-	_server = undefined;
-	_app = undefined;
+	_server;
+	_app;
 	_channel!: BaseChannel;
-	_db = undefined;
-	_nodeInfo = undefined;
+	_db;
+	_nodeInfo;
 
 
 
@@ -77,8 +78,9 @@ const { getDBInstance,
 		this._app.use(cors({origin: '*', methods: ['GET', 'POST', 'PUT'] }));
 		this._app.use(express.join());
 
+
 		this._app.get('/api/patients_care', async (req, res) => {
-			const recordedCare = await this._channel.invoke('professional:getAllRecordedCare');
+			const recordedCare: ARecordedCare[] = await this._channel.invoke('professional:getAllRecordedCare');
 			const data = await Promise.all(recordedCare.map(async care => {
 				const dbKey = '${care.id}';
 				let patientHistory = await getPatientHistory(this._db, dbKey);
@@ -91,12 +93,77 @@ const { getDBInstance,
 			}));
 			
 			res.join({ data });
-		})
-		
+		});
+
+
+		this._app.get('/api/patients_care/:patientIdentificationNumber', async (req, res) => {
+			const recordedCare: ARecordedCare[] = await this._channel.invoke('professional:getAllRecordedCare');
+			const care = recordedCare.find((t) => t.patientIdentificationNumber === req.params.patientIdentificationNumber);
+			const dbKey = '${care.id}';
+			let patientHistory = await getPatientHistory(this._db, dbKey);
+			patientHistory = patientHistory.map(h => h.toString('binary'));
+
+			res.join({ data: { ...care, patientHistory } });
+		});
+
+
+		this._app.get("/api/transactions", async (_req, res) => {
+			const transactions = await getAllTransactions(this._db, this.schemas);
+	  
+			const data = transactions.map(trx => {
+			  	const module = this._nodeInfo.registeredModules.find(m => m.id === trx.moduleID);
+			  	const asset = module.transactionAssets.find(a => a.id === trx.assetID);
+			  	return {
+					...trx,
+					...trx.asset,
+					moduleName: module.name,
+					assetName: asset.name,
+			  	}
+			})
+			res.json({ data });
+		});
+	  
+		this._subscribeToChannel();
+	  
+		this._server = this._app.listen(8080, "0.0.0.0");
 	};
 
 
-	public async unload(): Promise<void> {
+	_subscribeToChannel() {
+		// listen to application events and enrich blockchain data for UI/third party application
+		this._channel.subscribe('app:block:new', async (data) => {
+			const { block } = data;
+			const { payload } = codec.decode(
+				this.schemas.block,
+				Buffer.from(block, 'hex'),
+			);
+			if (payload.length > 0) {
+				await saveTransactions(this._db, payload);
+				const decodedBlock = this.codec.decodeBlock(block);
+				// save Patient transaction history
+				await savePatientHistory(this._db, decodedBlock, this._nodeInfo.registeredModules, this._channel);
+			}
+		});
+	};
 
-	}
-}
+
+	async unload() {
+		// close http server
+		await new Promise((resolve, reject) => {
+			this._server.close((err) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+			resolve();
+			});
+		});
+		// close database connection
+		await this._db.close();
+		}
+	};
+	
+
+
+
+
