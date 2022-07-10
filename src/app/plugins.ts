ea08c63c61635bf8e0c4cd33cfa74935f6f0c037
moussa <ehpmtra@gmail.com>
2022-07-10 18:58:37 +0200

    DashboardPlugin and FaucetPlugin added

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Application } from 'lisk-sdk';
-import { MedicalRecordPlugin } from "./plugins/medical_record/medical_record_plugin";
 
+import { MedicalRecordPlugin } from "./plugins/medical_record/medical_record_plugin";
 import { HTTPAPIPlugin } from 'lisk-sdk';
+
+import { DashboardPlugin } from '@liskhq/lisk-framework-dashboard-plugin';
+import { FaucetPlugin } from '@liskhq/lisk-framework-faucet-plugin';
 
 // ts-expect-error Unused variable error happens here until at least one module is registered
 export const registerPlugins = (app: Application): void => {
     app.registerPlugin(MedicalRecordPlugin);
     app.registerPlugin(HTTPAPIPlugin);
+    app.registerPlugin(DashboardPlugin);
+    app.registerPlugin(FaucetPlugin);
 };
