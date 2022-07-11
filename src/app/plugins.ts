8fa84fa39e5e3167d0eef37f19ee1b27d73d2c62
moussa <ehpmtra@gmail.com>
2022-07-11 03:24:11 +0200

    genesis block updated, bugs with DahsboardPlugin

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -5,12 +5,12 @@ import { MedicalRecordPlugin } from "./plugins/medical_record/medical_record_plu
 import { HTTPAPIPlugin } from 'lisk-sdk';
 
 import { DashboardPlugin } from '@liskhq/lisk-framework-dashboard-plugin';
-import { FaucetPlugin } from '@liskhq/lisk-framework-faucet-plugin';
+//import { FaucetPlugin } from '@liskhq/lisk-framework-faucet-plugin';
 
 // ts-expect-error Unused variable error happens here until at least one module is registered
 export const registerPlugins = (app: Application): void => {
     app.registerPlugin(MedicalRecordPlugin);
     app.registerPlugin(HTTPAPIPlugin);
     app.registerPlugin(DashboardPlugin);
-    app.registerPlugin(FaucetPlugin);
+    //app.registerPlugin(FaucetPlugin);
 };
