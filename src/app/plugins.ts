298823dda985af6204cbca64d50b496050c4f470
moussa <ehpmtra@gmail.com>
2022-07-11 16:18:16 +0200

    DashboardPlugin added

--- src/app/plugins.ts
+++ src/app/plugins.ts
@@ -5,14 +5,12 @@ import { MedicalRecordPlugin } from "./plugins/medical_record/medical_record_plu
 //import { HTTPAPIPlugin } from 'lisk-sdk';
 //import { ForgerPlugin } from 'lisk-sdk';
 
-//import { DashboardPlugin } from '@liskhq/lisk-framework-dashboard-plugin';
+import { DashboardPlugin } from '@liskhq/lisk-framework-dashboard-plugin';
 //import { FaucetPlugin } from '@liskhq/lisk-framework-faucet-plugin';
 
 // ts-expect-error Unused variable error happens here until at least one module is registered
 export const registerPlugins = (app: Application): void => {
     app.registerPlugin(MedicalRecordPlugin);
-    //app.registerPlugin(HTTPAPIPlugin);
-    //app.registerPlugin(ForgerPlugin)
-    //app.registerPlugin(DashboardPlugin);
+    app.registerPlugin(DashboardPlugin);
     //app.registerPlugin(FaucetPlugin);
 };
